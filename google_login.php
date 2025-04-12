5476358919cfaa823059b536db767d59af7705a2
Tanni <tannijannat61@gmail.com>
2025-04-13 01:09:21 +0600

    Update google_login.php

--- google_login.php
+++ google_login.php
@@ -1,1 +1,19 @@
+<?php
+require 'vendor/autoload.php'; // Ensure Google Client is loaded
+
+session_start();
+
+$client = new Google_Client();
+$client->setClientId('YOUR_CLIENT_ID_HERE');
+$client->setClientSecret('YOUR_CLIENT_SECRET_HERE');
+
+$client->setRedirectUri('http://localhost/BookStore/google_callback.php');
+$client->addScope("email");
+$client->addScope("profile");
+
+$login_url = $client->createAuthUrl();
+
+header("Location: $login_url");
+exit();
+?>
 
